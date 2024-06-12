9791a4c71bcaf4e7cc47cae062e929c244dd536d
William-silva-Developer <william958silva@gmail.com>
2024-06-12 20:51:55 -0300

    refactor: alterando icone favicon

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,127 +1,133 @@
-import React, { useEffect, useState } from "react";
-import { Container } from "../../../components/container";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { Container } from "../../../components/container"
+import { Link, useNavigate } from "react-router-dom"
 
-import logo from "../../../assets/logo.svg";
-import { Input } from "../../../components/input";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { auth } from "../../../services/firebase-connction";
-import { useDispatch } from "react-redux";
+import logo from "../../../../public/logo.svg"
+import { Input } from "../../../components/input"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { auth } from "../../../services/firebase-connction"
+import { useDispatch } from "react-redux"
 import {
-  authenticatedUser,
-  deauthenticatedUser,
-} from "../../../store/user-slice/userSlice";
-import { CustomLoading } from "../../../components/custom-loading/custom-loading";
+    authenticatedUser,
+    deauthenticatedUser,
+} from "../../../store/user-slice/userSlice"
+import { CustomLoading } from "../../../components/custom-loading/custom-loading"
 
 const schema = z.object({
-  email: z
-    .string()
-    .email("Insira um email válido")
-    .min(1, "O campo email é obrigatório"),
-  password: z.string().min(1, "O campo senha é obrigatório"),
-});
+    email: z
+        .string()
+        .email("Insira um email válido")
+        .min(1, "O campo email é obrigatório"),
+    password: z.string().min(1, "O campo senha é obrigatório"),
+})
 
-type FormData = z.infer<typeof schema>;
+type FormData = z.infer<typeof schema>
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate()
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
-  const [loadingAuth, setLoadingAuth] = useState<boolean>(false);
-  //----------------------------------------------------------------
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
-    mode: "onChange",
-  });
+    const [loadingAuth, setLoadingAuth] = useState<boolean>(false)
+    //----------------------------------------------------------------
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(schema),
+        mode: "onChange",
+    })
 
-  //----------------------------------------------------------------
-  useEffect(() => {
-    async function logoutUser() {
-      signOut(auth);
-      dispatch(deauthenticatedUser());
+    //----------------------------------------------------------------
+    useEffect(() => {
+        async function logoutUser() {
+            signOut(auth)
+            dispatch(deauthenticatedUser())
+        }
+        logoutUser()
+    }, [])
+    //----------------------------------------------------------------
+    function handleSubmitForm(data: FormData) {
+        setLoadingAuth(true)
+        signInWithEmailAndPassword(auth, data.email, data.password)
+            .then((user) => {
+                dispatch(
+                    authenticatedUser({
+                        uid: user.user.uid,
+                        name: user.user?.displayName,
+                        email: user.user?.email,
+                        isAutenticated: true,
+                    })
+                )
+                setLoadingAuth(false)
+                navigate("/dashboard", { replace: true })
+            })
+            .catch((err) => {
+                console.log("ERRO AO FAZER LOGIN")
+                console.log(err)
+                setLoadingAuth(false)
+            })
     }
-    logoutUser();
-  }, []);
-  //----------------------------------------------------------------
-  function handleSubmitForm(data: FormData) {
-    setLoadingAuth(true);
-    signInWithEmailAndPassword(auth, data.email, data.password)
-      .then((user) => {
-        dispatch(
-          authenticatedUser({
-            uid: user.user.uid,
-            name: user.user?.displayName,
-            email: user.user?.email,
-            isAutenticated: true,
-          })
-        );
-        setLoadingAuth(false);
-        navigate("/dashboard", { replace: true });
-      })
-      .catch((err) => {
-        console.log("ERRO AO FAZER LOGIN");
-        console.log(err);
-        setLoadingAuth(false);
-      });
-  }
 
-  return (
-    <Container>
-      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
-        <img src={logo} alt="Logo do site" className="w-full mb-6 max-w-sm" />
+    return (
+        <Container>
+            <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+                <img
+                    src={logo}
+                    alt="Logo do site"
+                    className="w-full mb-6 max-w-sm"
+                />
 
-        <form
-          onSubmit={handleSubmit(handleSubmitForm)}
-          className="bg-white max-w-xl w-full rounded-lg p-4"
-        >
-          <div className="mb-3">
-            <Input
-              type="email"
-              placeholder="Digite seu email..."
-              name="email"
-              register={register}
-              error={errors.email?.message}
-            />
-          </div>
+                <form
+                    onSubmit={handleSubmit(handleSubmitForm)}
+                    className="bg-white max-w-xl w-full rounded-lg p-4"
+                >
+                    <div className="mb-3">
+                        <Input
+                            type="email"
+                            placeholder="Digite seu email..."
+                            name="email"
+                            register={register}
+                            error={errors.email?.message}
+                        />
+                    </div>
 
-          <div className="mb-3">
-            <Input
-              type="password"
-              placeholder="Digite seu senha..."
-              name="password"
-              register={register}
-              error={errors.password?.message}
-            />
-          </div>
+                    <div className="mb-3">
+                        <Input
+                            type="password"
+                            placeholder="Digite seu senha..."
+                            name="password"
+                            register={register}
+                            error={errors.password?.message}
+                        />
+                    </div>
 
-          {loadingAuth ? (
-            <div className="items-center justify-center flex flex-col">
-              <CustomLoading type="spin" color="#00ff00" />
-              <p className="font-semibold text-slate-700">Aguarde...</p>
+                    {loadingAuth ? (
+                        <div className="items-center justify-center flex flex-col">
+                            <CustomLoading type="spin" color="#00ff00" />
+                            <p className="font-semibold text-slate-700">
+                                Aguarde...
+                            </p>
+                        </div>
+                    ) : (
+                        <button
+                            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+                            type="submit"
+                        >
+                            Acessar
+                        </button>
+                    )}
+                </form>
+                <Link to={"/register"}>
+                    <p>Ainda não possui uma conta? cadastre-se</p>
+                </Link>
             </div>
-          ) : (
-            <button
-              className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
-              type="submit"
-            >
-              Acessar
-            </button>
-          )}
-        </form>
-        <Link to={"/register"}>
-          <p>Ainda não possui uma conta? cadastre-se</p>
-        </Link>
-      </div>
-    </Container>
-  );
-};
+        </Container>
+    )
+}
 
-export { Login };
+export { Login }
--- src/store/user-slice/userSlice.ts
+++ src/store/user-slice/userSlice.ts
@@ -1,45 +1,44 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { IUserProps } from "../../interfaces/user";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { IUserProps } from "../../interfaces/user"
 
 const initialState = {
-  user: {
-    uid: "",
-    name: "",
-    email: "",
-    isAutenticated: false,
-  } as IUserProps,
-  loadingAuth: false,
-};
+    user: {
+        uid: "",
+        name: "",
+        email: "",
+        isAutenticated: false,
+    } as IUserProps,
+    loadingAuth: false,
+}
 
 const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    authenticatedUser: (state, action: PayloadAction<IUserProps>) => {
-      console.log("Slice: ", action.payload);
-      return {
-        ...state,
-        user: {
-          uid: action.payload.uid,
-          name: action.payload.name,
-          email: action.payload.email,
-          isAutenticated: action.payload.isAutenticated,
+    name: "user",
+    initialState,
+    reducers: {
+        authenticatedUser: (state, action: PayloadAction<IUserProps>) => {
+            return {
+                ...state,
+                user: {
+                    uid: action.payload.uid,
+                    name: action.payload.name,
+                    email: action.payload.email,
+                    isAutenticated: action.payload.isAutenticated,
+                },
+            }
         },
-      };
-    },
-    deauthenticatedUser: (state) => {
-      return {
-        ...state,
-        user: {
-          uid: "",
-          name: "",
-          email: "",
-          isAutenticated: false,
+        deauthenticatedUser: (state) => {
+            return {
+                ...state,
+                user: {
+                    uid: "",
+                    name: "",
+                    email: "",
+                    isAutenticated: false,
+                },
+            }
         },
-      };
     },
-  },
-});
+})
 
-export const { authenticatedUser, deauthenticatedUser } = userSlice.actions;
-export default userSlice.reducer;
+export const { authenticatedUser, deauthenticatedUser } = userSlice.actions
+export default userSlice.reducer
